038e53d1b3f89df66a6e64d597c72e42124c2e00
SamboHassan <sambohasan@gmail.com>
2025-03-04 06:13:46 +0100

    add sidebar context management

--- chat-gpt-clone/src/App.tsx
+++ chat-gpt-clone/src/App.tsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Box, Flex, Stack } from "@chakra-ui/react";
+import { BottomSection } from "./bottom-section";
+import { MiddleSection } from "./middle-section";
+import { Sidebar } from "./sidebar";
+import { SidebarProvider } from "./sidebar-context";
+import { TopSection } from "./top-section";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <SidebarProvider>
+      <Flex minH="100dvh">
+        <Sidebar />
+
+        <Box flex="1">
+          <Stack h="full">
+            <TopSection />
+            <MiddleSection />
+            <BottomSection />
+          </Stack>
+        </Box>
+      </Flex>
+    </SidebarProvider>
+  );
 }
 
-export default App
+export default App;
